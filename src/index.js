a06985479fe85b03c431723c979f5523697b4fd5
Nardelys15c <136908202+Nardelys15c@users.noreply.github.com>
2024-01-14 18:00:20 -0500

    edit prompt

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generateRecipe(event){
     
 let instructionsInput= document.querySelector("#user-instructions");
     let apiKey = "7bf4e319208b1e35aafo3ce425b6c3at";
-    let context = "You are a famous crockpot chef and you can provide a quick and easy recipe. Your mission is to provide quick and easy crockpot recipe using basic HTML and seperate each line with a <br />. make sure to follow the user instructions. Sign the Recipe with SheCodes AI.";
+    let context = "You are a famous crockpot chef and you can provide a quick and easy recipe. Your mission is to provide a quick and easy crockpot recipe using basic HTML and seperate each line with a <br />. make sure to follow the user instructions. Sign the Recipe with SheCodes AI.";
     let prompt=`User instructions are: Generate a crockpot recipe using ${instructionsInput.value}`;
     let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
